f37e4370fe7b91ad49d89d31afe69fb345879862
memory4963 <bolo4963@gmail.com>
2023-01-05 10:46:26 +0900

    [feat] "cc without lrp" model

--- compressai/models/CC.py
+++ compressai/models/CC.py
@@ -1271,3 +1271,297 @@ class CC_manual_prune(CC):
             if state_dict[k].shape == cur_state_dict[k].shape:
                 cur_state_dict[k] = state_dict[k]
         super().load_state_dict(cur_state_dict)
+
+
+class CCWithoutLRP(CompressionModel):
+    """Channel-wise Context model"""
+
+    def __init__(self, N=192, M=320, num_slices=10, max_support_slices=-1, **kwargs):
+        super().__init__(**kwargs)
+        self.num_slices = num_slices
+        self.max_support_slices = max_support_slices
+
+        self.g_a = nn.Sequential(
+            conv(3, N),
+            GDN(N),
+            conv(N, N),
+            GDN(N),
+            conv(N, N),
+            GDN(N),
+            conv(N, M),
+        )
+
+        self.g_s = nn.Sequential(
+            deconv(M, N),
+            GDN(N, inverse=True),
+            deconv(N, N),
+            GDN(N, inverse=True),
+            deconv(N, N),
+            GDN(N, inverse=True),
+            deconv(N, 3),
+        )
+
+        self.h_a = nn.Sequential(
+            conv3x3(M, M),
+            nn.ReLU(),
+            conv(M, 256, stride=2),
+            nn.ReLU(),
+            conv(256, 192, stride=2),
+        )
+
+        self.h_mean_s = nn.Sequential(
+            deconv(192, 192, stride=2),
+            nn.ReLU(),
+            deconv(192, 256, stride=2),
+            nn.ReLU(),
+            conv3x3(256, M),
+        )
+
+        self.h_scale_s = nn.Sequential(
+            deconv(192, 192, stride=2),
+            nn.ReLU(),
+            deconv(192, 256, stride=2),
+            nn.ReLU(),
+            conv3x3(256, M),
+        )
+        self.cc_mean_transforms = nn.ModuleList(
+            nn.Sequential(
+                conv3x3(M + M//self.num_slices*i, 224 + M//self.num_slices*i*2//3),
+                nn.ReLU(),
+                conv3x3(224 + M//self.num_slices*i*2//3, 128 + M//self.num_slices*i*1//3),
+                nn.ReLU(),
+                conv3x3(128 + M//self.num_slices*i*1//3, M//self.num_slices),
+            ) for i in range(self.num_slices)
+        )
+        self.cc_scale_transforms = nn.ModuleList(
+            nn.Sequential(
+                conv3x3(M + M//self.num_slices*i, 224 + M//self.num_slices*i*2//3),
+                nn.ReLU(),
+                conv3x3(224 + M//self.num_slices*i*2//3, 128 + M//self.num_slices*i*1//3),
+                nn.ReLU(),
+                conv3x3(128 + M//self.num_slices*i*1//3, M//self.num_slices),
+            ) for i in range(self.num_slices)
+            )
+        # self.lrp_transforms = nn.ModuleList(
+        #     nn.Sequential(
+        #         conv3x3(M + M//self.num_slices*(i+1), 224 + M//self.num_slices*i*2//3),
+        #         nn.ReLU(),
+        #         conv3x3(224 + M//self.num_slices*i*2//3, 128 + M//self.num_slices*i*1//3),
+        #         nn.ReLU(),
+        #         conv3x3(128 + M//self.num_slices*i*1//3, M//self.num_slices),
+        #     ) for i in range(self.num_slices)
+        # )
+
+        self.entropy_bottleneck = EntropyBottleneck(N)
+        self.gaussian_conditional = GaussianConditional(None)
+        self.during_time = 0.
+
+
+    def update(self, scale_table=None, force=False):
+        if scale_table is None:
+            scale_table = get_scale_table()
+        updated = self.gaussian_conditional.update_scale_table(scale_table, force=force)
+        updated |= super().update(force=force)
+        return updated
+
+
+    def forward(self, x):
+        y = self.g_a(x)
+        y_shape = y.shape[2:]
+        z = self.h_a(y)
+        _, z_likelihoods = self.entropy_bottleneck(z)
+
+        # Use rounding (instead of uniform noise) to modify z before passing it
+        # to the hyper-synthesis transforms. Note that quantize() overrides the
+        # gradient to create a straight-through estimator.
+        z_offset = self.entropy_bottleneck._get_medians()
+        z_tmp = z - z_offset
+        z_hat = ste_round(z_tmp) + z_offset
+
+        latent_scales = self.h_scale_s(z_hat)
+        latent_means = self.h_mean_s(z_hat)
+
+        y_slices = self.split_slices(y)
+        y_hat_slices = []
+        y_likelihood = []
+
+        for slice_index, y_slice in enumerate(y_slices):
+            support_slices = (y_hat_slices if self.max_support_slices < 0 else y_hat_slices[:self.max_support_slices])
+            mean_support = torch.cat([latent_means] + support_slices, dim=1)
+            mu = self.cc_mean_transforms[slice_index](mean_support)
+            mu = mu[:, :, :y_shape[0], :y_shape[1]]
+
+            scale_support = torch.cat([latent_scales] + support_slices, dim=1)
+            scale = self.cc_scale_transforms[slice_index](scale_support)
+            scale = scale[:, :, :y_shape[0], :y_shape[1]]
+
+            _, y_slice_likelihood = self.gaussian_conditional(y_slice, scale, mu)
+            y_likelihood.append(y_slice_likelihood)
+            y_hat_slice = ste_round(y_slice - mu) + mu
+
+            # lrp_support = torch.cat([mean_support, y_hat_slice], dim=1)
+            # lrp = self.lrp_transforms[slice_index](lrp_support)
+            # lrp = 0.5 * torch.tanh(lrp)
+            # y_hat_slice += lrp
+
+            y_hat_slices.append(y_hat_slice)
+
+        y_hat = torch.cat(y_hat_slices, dim=1)
+        y_likelihoods = torch.cat(y_likelihood, dim=1)
+        x_hat = self.g_s(y_hat)
+
+        return {
+            "x_hat": x_hat,
+            "likelihoods": {"y": y_likelihoods, "z": z_likelihoods},
+        }
+    
+    def split_slices(self, y):
+        return y.chunk(self.num_slices, 1)
+
+    def load_state_dict(self, state_dict):
+        update_registered_buffers(
+            self.gaussian_conditional,
+            "gaussian_conditional",
+            ["_quantized_cdf", "_offset", "_cdf_length", "scale_table"],
+            state_dict,
+        )
+        super().load_state_dict(state_dict)
+
+    @classmethod
+    def from_state_dict(cls, state_dict):
+        """Return a new model instance from `state_dict`."""
+        N = state_dict["g_a.0.weight"].size(0)
+        M = state_dict["g_a.6.weight"].size(0)
+        slice_num = 0
+        while (f'cc_mean_transforms.{slice_num}.0.weight' in state_dict): slice_num+=1
+        net = cls(N, M, slice_num)
+        net.load_state_dict(state_dict)
+        return net
+
+    def compress(self, x):
+        y = self.g_a(x)
+        y_shape = y.shape[2:]
+
+        z = self.h_a(y)
+        z_strings = self.entropy_bottleneck.compress(z)
+        z_hat = self.entropy_bottleneck.decompress(z_strings, z.size()[-2:])
+
+        latent_scales = self.h_scale_s(z_hat)
+        latent_means = self.h_mean_s(z_hat)
+
+        y_slices = self.split_slices(y)
+        y_hat_slices = []
+        y_scales = []
+        y_means = []
+
+        cdf = self.gaussian_conditional.quantized_cdf.tolist()
+        cdf_lengths = self.gaussian_conditional.cdf_length.reshape(-1).int().tolist()
+        offsets = self.gaussian_conditional.offset.reshape(-1).int().tolist()
+
+        encoder = BufferedRansEncoder()
+        symbols_list = []
+        indexes_list = []
+        y_strings = []
+
+        for slice_index, y_slice in enumerate(y_slices):
+            support_slices = (y_hat_slices if self.max_support_slices < 0 else y_hat_slices[:self.max_support_slices])
+
+            mean_support = torch.cat([latent_means] + support_slices, dim=1)
+            mu = self.cc_mean_transforms[slice_index](mean_support)
+            mu = mu[:, :, :y_shape[0], :y_shape[1]]
+
+            scale_support = torch.cat([latent_scales] + support_slices, dim=1)
+            scale = self.cc_scale_transforms[slice_index](scale_support)
+            scale = scale[:, :, :y_shape[0], :y_shape[1]]
+
+            index = self.gaussian_conditional.build_indexes(scale)
+            y_q_slice = self.gaussian_conditional.quantize(y_slice, "symbols", mu)
+            y_hat_slice = y_q_slice + mu
+
+            symbols_list.extend(y_q_slice.reshape(-1).tolist())
+            indexes_list.extend(index.reshape(-1).tolist())
+
+
+            lrp_support = torch.cat([mean_support, y_hat_slice], dim=1)
+            lrp = self.lrp_transforms[slice_index](lrp_support)
+            lrp = 0.5 * torch.tanh(lrp)
+            y_hat_slice += lrp
+
+            y_hat_slices.append(y_hat_slice)
+            y_scales.append(scale)
+            y_means.append(mu)
+
+        torch.cuda.synchronize()
+        start = time()
+        encoder.encode_with_indexes(symbols_list, indexes_list, cdf, cdf_lengths, offsets)
+        y_string = encoder.flush()
+        torch.cuda.synchronize()
+        self.during_time += time()-start
+        y_strings.append(y_string)
+
+        return {"strings": [y_strings, z_strings], "shape": z.size()[-2:]}
+
+    def _likelihood(self, inputs, scales, means=None):
+        half = float(0.5)
+        if means is not None:
+            values = inputs - means
+        else:
+            values = inputs
+
+        scales = torch.max(scales, torch.tensor(0.11))
+        values = torch.abs(values)
+        upper = self._standardized_cumulative((half - values) / scales)
+        lower = self._standardized_cumulative((-half - values) / scales)
+        likelihood = upper - lower
+        return likelihood
+
+    def _standardized_cumulative(self, inputs):
+        half = float(0.5)
+        const = float(-(2 ** -0.5))
+        # Using the complementary error function maximizes numerical precision.
+        return half * torch.erfc(const * inputs)
+
+    def decompress(self, strings, shape):
+        z_hat = self.entropy_bottleneck.decompress(strings[1], shape)
+        latent_scales = self.h_scale_s(z_hat)
+        latent_means = self.h_mean_s(z_hat)
+
+        y_shape = [z_hat.shape[2] * 4, z_hat.shape[3] * 4]
+
+        y_string = strings[0][0]
+
+        y_hat_slices = []
+        cdf = self.gaussian_conditional.quantized_cdf.tolist()
+        cdf_lengths = self.gaussian_conditional.cdf_length.reshape(-1).int().tolist()
+        offsets = self.gaussian_conditional.offset.reshape(-1).int().tolist()
+
+        decoder = RansDecoder()
+        decoder.set_stream(y_string)
+
+        for slice_index in range(self.num_slices):
+            support_slices = (y_hat_slices if self.max_support_slices < 0 else y_hat_slices[:self.max_support_slices])
+            mean_support = torch.cat([latent_means] + support_slices, dim=1)
+            mu = self.cc_mean_transforms[slice_index](mean_support)
+            mu = mu[:, :, :y_shape[0], :y_shape[1]]
+
+            scale_support = torch.cat([latent_scales] + support_slices, dim=1)
+            scale = self.cc_scale_transforms[slice_index](scale_support)
+            scale = scale[:, :, :y_shape[0], :y_shape[1]]
+
+            index = self.gaussian_conditional.build_indexes(scale)
+
+            rv = decoder.decode_stream(index.reshape(-1).tolist(), cdf, cdf_lengths, offsets)
+            rv = torch.Tensor(rv).reshape(1, -1, y_shape[0], y_shape[1])
+            y_hat_slice = self.gaussian_conditional.dequantize(rv, mu)
+
+            lrp_support = torch.cat([mean_support, y_hat_slice], dim=1)
+            lrp = self.lrp_transforms[slice_index](lrp_support)
+            lrp = 0.5 * torch.tanh(lrp)
+            y_hat_slice += lrp
+
+            y_hat_slices.append(y_hat_slice)
+
+        y_hat = torch.cat(y_hat_slices, dim=1)
+        x_hat = self.g_s(y_hat).clamp_(0, 1)
+
+        return {"x_hat": x_hat}
--- compressai/zoo/__init__.py
+++ compressai/zoo/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 
-from compressai.models import CCResRepWithoutY, CCEnhancedResRep, CC_manual_prune, CCResRepPruned, CCResRep, CC_RandomSplit, CC_uneven, CC_GD, SymmetricalTransFormer, WACNN, TransformerBasedCoding, DYSTF, CC
+from compressai.models import CCResRepWithoutY, CCEnhancedResRep, CC_manual_prune, CCResRepPruned, CCResRep, CC_RandomSplit, CC_uneven, CC_GD, SymmetricalTransFormer, WACNN, TransformerBasedCoding, DYSTF, CC, CCWithoutLRP
 
 from .pretrained import load_pretrained as load_state_dict
 
@@ -31,4 +31,5 @@ models = {
     'cc_manual_prune': CC_manual_prune,
     'cc_enhanced_resrep': CCEnhancedResRep,
     'cc_resrep_without_y': CCResRepWithoutY,
+    'cc_without_lrp': CCWithoutLRP,
 }
